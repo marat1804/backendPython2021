99f1723117c9124fde99d498f4c237bd0d6182bf
Marat Khisamutdinov <maratbest6@gmail.com>
2021-10-22 11:42:14 +0300

    add: += -= radd, rsub

--- homework2/task1/custom_list.py
+++ homework2/task1/custom_list.py
@@ -19,6 +19,19 @@ class CustomList(list):
             result.append(first_elem + second_elem)
         return result
 
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        max_len = max(len(self), len(other))
+        if len(self) != max_len:
+            while len(self) != max_len:
+                self.append(0)
+        for i in range(max_len):
+            second_elem = other[i] if i < len(other) else 0
+            self[i] = self[i] + second_elem
+        return self
+
     def __sub__(self, other):
         result = CustomList()
         max_len = max(len(self), len(other))
@@ -27,6 +40,19 @@ class CustomList(list):
             second_elem = other[i] if i < len(other) else 0
             result.append(first_elem - second_elem)
         return result
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __isub__(self, other):
+        max_len = max(len(self), len(other))
+        if len(self) != max_len:
+            while len(self) != max_len:
+                self.append(0)
+        for i in range(max_len):
+            second_elem = other[i] if i < len(other) else 0
+            self[i] = self[i] - second_elem
+        return self
 
     def __lt__(self, other):
         return sum(self) < sum(other)
