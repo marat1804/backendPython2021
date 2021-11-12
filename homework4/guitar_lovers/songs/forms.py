4f79c266d47aa7e8717bbdba356e0695379e4f0d
Marat Khisamutdinov <maratbest6@gmail.com>
2021-11-12 14:36:30 +0300

    CRUD for author and genre

--- homework4/guitar_lovers/songs/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class AddSongForm(forms.Form):
-    name = forms.CharField(max_length=64)
-    date = forms.DateField(required=False)
-    text = forms.CharField(max_length=1024, required=False)
-    genre_id = forms.IntegerField(required=True)
-    author_id = forms.IntegerField(required=True)
